221a2ff4a9fc2210acdef1db76410fc4195244c4
Holden Adamec <holden.adamec@gmail.com>
2017-06-13 11:53:19 -0700

    Added loading screen

--- src/tech/hackerlife/game/Game.java
+++ src/tech/hackerlife/game/Game.java
@@ -14,14 +14,18 @@ public class Game {
 	public static int counter = 0, fps = 0, playerSpeed;
 	static Level level;
 	static Player player;
-	static Turtle turtle;
+	static int tuts = 10;
+	static Turtle[] turtle = new Turtle[tuts];
 
 	public static void init() {
+		Main.f.getGraphics().fillRect(0, 0, Main.WIDTH, Main.HEIGHT);
 		tileSize = (int) (Main.HEIGHT / 9);
 		Point spawn = new Point(10 * tileSize, 7 * tileSize);
 		level = new Level();
 		player = new Player(spawn);
-		turtle = new Turtle();
+		for(int i = 0; i < tuts; i++) {
+			turtle[i] = new Turtle();
+		}
 		mapOrigin = new Point(spawn);
 		playerSpeed = (int)(Main.HEIGHT / 200);
 	}
@@ -29,13 +33,15 @@ public class Game {
 	public static void update(Graphics g, int width, int height) {
 		Keyboard.update();
 		
-		player.update(mapOrigin);
+		player.update();
 
 		level.update(g, tileSize, mapOrigin);
 		
-		player.render(g);
+		for(int k = 0; k < tuts; k++) {
+			turtle[k].update(g);
+		}
 		
-		turtle.update(g);
+		player.render(g);
 		
 		if (Main.displayFPS) fpsCounter(g);
 	}
